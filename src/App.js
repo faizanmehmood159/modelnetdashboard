8060307ffb107b442ebcb13eae94dbaddbd06136
Ehtisham Ul Haq <125582602+EhtishamUlHaq265@users.noreply.github.com>
2024-02-14 15:35:35 +0500

    chnages

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import initializeApp from './app/init';
 // Importing pages
 const Layout = lazy(() => import('./containers/Layout'))
 const Login = lazy(() => import('./pages/Login'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
-const Register = lazy(() => import('./pages/Register'))
 const Documentation = lazy(() => import('./pages/Documentation'))
 
 
@@ -34,8 +32,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/documentation" element={<Documentation />} />
           
           {/* Place new routes over this */}
--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,6 +1,5 @@
 import {useState, useRef} from 'react'
 import {Link} from 'react-router-dom'
-import LandingIntro from './LandingIntro'
 import ErrorText from  '../../components/Typography/ErrorText'
 import InputText from '../../components/Input/InputText'
 
@@ -40,8 +39,9 @@ function Login(){
         <div className="min-h-screen bg-base-200 flex items-center">
             <div className="card mx-auto w-full max-w-5xl  shadow-xl">
                 <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
-                <div className=''>
-                        <LandingIntro />
+               
+                <div className='py-24 px-10'>
+                    <h2 className='text-2xl font-semibold mb-2 text-center'>Model net</h2>
                 </div>
                 <div className='py-24 px-10'>
                     <h2 className='text-2xl font-semibold mb-2 text-center'>Login</h2>
@@ -55,13 +55,8 @@ function Login(){
 
                         </div>
 
-                        <div className='text-right text-primary'><Link to="/forgot-password"><span className="text-sm  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Forgot Password?</span></Link>
-                        </div>
-
                         <ErrorText styleClass="mt-8">{errorMessage}</ErrorText>
                         <button type="submit" className={"btn mt-2 w-full btn-primary" + (loading ? " loading" : "")}>Login</button>
-
-                        <div className='text-center mt-4'>Don't have an account yet? <Link to="/register"><span className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Register</span></Link></div>
                     </form>
                 </div>
             </div>

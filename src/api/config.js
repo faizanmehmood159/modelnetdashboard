0efdfc0e96b3369586c07827fc2e5d9254f6313a
Faizan Mehmood <faizanmehmood159@gmail.com>
2024-05-15 21:58:58 +0500

    mkj

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@
 
 // export const API_BASE_URL_USER = "http://192.168.1.9:4500/api/v1/auth/";
 // export const API_BASE_URL_COMPANY = "http://192.168.1.9:4500/api/v1/admin/";
-// export const API_BASE_URL = "http://192.168.1.9:4500/api/v1/auth/";
+// export const API_BASE_URL = "http://192.168.1.59:4500/api/v1/auth/";
 
 
-export const API_AUTH_URL = "http://192.168.38.237:8000/api/v1/auth/";
-export const API_URL = "http://192.168.38.237:8000/api/v1/";
+export const API_AUTH_URL = "http://192.168.1.5:8000/api/v1/auth/";
+export const API_URL = "http://192.168.1.5:8000/api/v1/";
